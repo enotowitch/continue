643f233fad9d66fa0978d71948ec99196c3b4031
enotowitch <en.enotowitch@gmail.com>
2021-05-24 13:54:42 +0300

    24.05 +log-as session_user logo

--- log-as.php
+++ log-as.php
@@ -1,6 +1,11 @@
 <? if($_SESSION["user"] != NULL): ?>
-	<div class="log-as">
-	<div>You logged as: <span><? echo $_SESSION["user"]["mail"]; ?></span></div>
-		<a class="out" href="logout.php">LOG OUT</a>
+
+<div class="log-as">
+	<div class="img-mail">
+		<img class="log-as-img" src="<? echo $_SESSION["user"]["logo"]; ?>">
+		<div class="log-as-mail"><? echo $_SESSION["user"]["mail"]; ?></div>
 	</div>
+	<a class="out" href="logout.php">LOG OUT</a>
+</div>
+
 <? endif; ?>
